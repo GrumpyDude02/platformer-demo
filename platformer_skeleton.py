b922ef996d0306ba48888e4c448673866dffb7f0
Youssef Bakouch <108092782+GrumpyDude02@users.noreply.github.com>
2022-06-28 16:15:40 +0100

    Update platformer_skeleton.py

--- platformer_skeleton.py
+++ platformer_skeleton.py
@@ -1,32 +1,35 @@
-
-from mimetypes import suffix_map
-from re import T
 from pygame.math import Vector2
 import pygame,sys
 
-width=600
+width=1024
 height=600
 tile_size=64
 RED=(255,0,0)
 camera_x=0
 camera_y=0
+lastpos=0
+lastpos_y=0
 
 pygame.init()
 screen=pygame.display.set_mode((width,height))
 clock=pygame.time.Clock()
 
 
-map=[[0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,1,1,1,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1]]
+map=[
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    ]
 
 class player():
     def __init__(self,x,y):
@@ -36,13 +39,16 @@ class player():
         self.friction=-0.2
         self.acc=Vector2(0,0.6)
         self.speed=Vector2(0,0)
+        self.surface=pygame.Surface((tile_size//2,tile_size))
         self.hitbox=pygame.Rect(self.pos.x,self.pos.y,tile_size//2,tile_size)
         self.jump=False
         self.jumpcounter=2
 
     def draw_player(self,window):
         self.move_player()
-        pygame.draw.rect(window,pygame.Color('green'),self.hitbox)
+        self.surface.fill((0,255,0))
+        #pygame.draw.rect(window,pygame.Color('green'),self.hitbox)
+        window.blit(self.surface,(self.hitbox.x-camera_x,self.hitbox.y-camera_y))
     
     def apply_gravity(self):
         #Y axis
@@ -94,26 +100,31 @@ class player():
                     self.speed.x=0
                     self.acc.x=0
                     self.hitbox.left=rects.right        
-            
-
 
 def draw_map(map,window,char):
     global camera_x,camera_y
-    hit_list=[]
-    surface=pygame.Surface((1200,1200))
+    x=-char.hitbox.centerx + width//2
+    y=-char.hitbox.centery + height//2
+    surface=pygame.Surface((2000,2000))
     surface.set_colorkey((0,0,0))
+    x=min(0,x)
+    y=min(0,y)
+    x=max(-(len(map[0])*tile_size-width),x)
+    y=max(-(len(map)*tile_size-height),y)
+    camera_x-=(x+camera_x)//20
+    camera_y-=(y+camera_y)//20  
+    hit_list=[]
     for i in range(len(map)):
-        for j in range(len(map[i])):
-            if map[i][j]:
+        for j in range(len(map[0])):
+            if map[i][j]==1:
                 rect=pygame.Rect((j)*tile_size,i*tile_size,tile_size,tile_size)
                 hit_list.append(rect)
                 pygame.draw.rect(surface,RED,rect)
-                rect.x-=camera_x
-    window.blit(surface,(0-camera_x,0))
+    window.blit(surface,(0-camera_x,0-camera_y))
     return hit_list
 
 
-player1=player(300,100)
+player1=player(600,600)
 
 
 def game(window,clock):
@@ -131,8 +142,8 @@ def game(window,clock):
                 if event.key==pygame.K_UP:
                     print("released")
                     if player1.jumpcounter==1:
-                       if player1.speed.y<-5: 
-                            player1.speed.y=-5
+                       if player1.speed.y<-2: 
+                            player1.speed.y=-2
         screen.fill(pygame.Color('white'))
         hits=draw_map(map,screen,player1)
         player1.draw_player(screen)
